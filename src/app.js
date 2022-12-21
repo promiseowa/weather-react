a1e0b01db4c90f37236030201e2ebf060e13d0ae
promise <108261992+promiseowa@users.noreply.github.com>
2022-12-21 07:15:13 +0100

    debugging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Weather from "./Weather";
+import Weather from "./weather";
 
 export default function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
-import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
