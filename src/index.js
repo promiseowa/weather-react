339ef9c66a0964f18d5898e98e1c705a2dcfc043
promise <108261992+promiseowa@users.noreply.github.com>
2022-12-21 07:01:41 +0100

    updated app.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
